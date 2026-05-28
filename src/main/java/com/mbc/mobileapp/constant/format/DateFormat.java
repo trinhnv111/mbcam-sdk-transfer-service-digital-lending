0bbf1624db6f60c8f5681466be7a6fb201b37b32
POJO <nvtrinh2407@gmail.com>
2026-05-28 11:38:33 +0700

    merge code

--- src/main/java/com/mbc/mobileapp/constant/format/DateFormat.java
+++
@@ -1,7 +0,0 @@
-package com.mbc.mobileapp.constant.format;
-
-import java.text.SimpleDateFormat;
-
-public class DateFormat {
-    public static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-}
--- src/main/java/com/mbc/mobileapp/rest/user/pincode/SetPinCodeResponse.java
+++
@@ -1,9 +0,0 @@
-package com.mbc.mobileapp.rest.user.pincode;
-
-import com.mbc.common.rest.bean.BaseResponse;
-import lombok.*;
-
-@Getter
-@Setter
-public class SetPinCodeResponse extends BaseResponse {
-}
